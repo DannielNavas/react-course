3c539cd2adf6d765d5e3339d3231c20ee71050ca
DannielNavas <dannielnavas@gmail.com>
2023-10-03 08:30:13 -0500

    ✨ fetch api

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-export const Card = () => {
+const Card = (data) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          Electronics
+          {data.category.name}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src={data.images[0]}
           alt="Headphones"
         />
         <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
@@ -17,9 +17,12 @@ export const Card = () => {
         </button>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light">{data.title}</span>
+        <span className="text-lg font-medium">{data.price}</span>
       </p>
     </div>
   );
 };
+
+
+export default Card;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
 import Card from "../../Components/Card";
 import Layaut from "../../Components/Layout";
 
+import { useEffect, useState } from "react";
+
+export const apiUrl = "https://api.escuelajs.co/api/v1";
+
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/products`)
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }
+    , []
+  );
   return (
     <Layaut>
       <h1>Home</h1>
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items && items.map((item) => <Card key={item.id} {...item} />)}
+      </div>
+      {/* <Card /> */}
     </Layaut>
   );
 }
