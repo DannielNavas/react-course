e6abadaeee72fa769dffc2c52ae48865d6e1506f
DannielNavas <dannielnavas@gmail.com>
2023-10-09 08:38:52 -0500

    ✨ show detail product

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -6,7 +6,6 @@ import "./style.css";
 
 const ProductDetail = () => {
   const context = useContext(ShoppingCartContext);
-
   return (
     <aside
       className={`${
@@ -22,6 +21,25 @@ const ProductDetail = () => {
           ></XMarkIcon>
         </div>
       </div>
+
+      <figure className="px-6">
+        <img
+          className="w-full h-full rounded-lg"
+          src={context.productShow?.images[0]}
+          alt={context.productShow.title}
+        />
+      </figure>
+      <p className="flex flex-col  p-6">
+        <span className="font-medium text-md">
+          <strong>Product:</strong> {context.productShow.title}
+        </span>
+        <span className="font-medium text-2xl mb-2">
+          <strong>Price:</strong> ${context.productShow.price}
+        </span>
+        <span className="font-light text-sm">
+          <strong>Description:</strong> {context.productShow.description}
+        </span>
+      </p>
     </aside>
   );
 };
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,6 +5,8 @@ export const ShoppingCartContext = createContext();
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  // PrdocutDetail show product detail
+  const [productShow, setProductShow] = useState({});
 
   const openProductDetail = () => setIsProductDetailOpen(true);
   const closeProductDetail = () => setIsProductDetailOpen(false);
@@ -16,6 +18,8 @@ export const ShoppingCartProvider = ({ children }) => {
       openProductDetail,
       closeProductDetail,
       isProductDetailOpen,
+      productShow,
+      setProductShow,
     }} >
       {children}
     </ShoppingCartContext.Provider>
